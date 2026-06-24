36dc5b21bb4cfc22cbb14ccfb9a053f2f2f7669b
trseries1234 <trseries1234@gmail.com>
2026-06-24 23:27:43 +0530

    Sync LeetCode solution: Sqrt(x) (C++)

---
+++ Easy/0069_Sqrtx.cpp
@@ -0,0 +1,17 @@
+/*
+ * Problem: Sqrt(x)
+ * Problem ID: 69
+ * Difficulty: Easy
+ * Language: C++
+ * Runtime: 0 ms
+ * Memory: 8.5 MB
+ * Synced From: LeetCode
+ * Date: 2026-06-24
+ */
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        return sqrt(x);
+    }
+};
