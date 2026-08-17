5227d75dbd9c3f10d8e2185d66c80914042274f2
trseries1234 <trseries1234@gmail.com>
2026-08-17 11:11:18 +0530

    Sync LeetCode solution: Two Sum (C++)

--- Easy/0001_Two_Sum.cpp
+++ Easy/0001_Two_Sum.cpp
@@ -3,27 +3,35 @@
  * Problem ID: 1
  * Difficulty: Easy
  * Language: C++
- * Runtime: 2 ms
- * Memory: 14.9 MB
+ * Runtime: 0 ms
+ * Memory: 14.8 MB
  * Synced From: LeetCode
- * Date: 2026-06-24
+ * Date: 2026-08-17
  */
+
+#include <vector>
+#include <unordered_map>
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> num_map;
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        // Maps the number value to its index
+        std::unordered_map<int, int> numMap;
         
         for (int i = 0; i < nums.size(); ++i) {
             int complement = target - nums[i];
             
-            if (num_map.find(complement) != num_map.end()) {
-                return {num_map[complement], i};
+            // Check if the complement already exists in the map
+            if (numMap.find(complement) != numMap.end()) {
+                // Return the indices of the complement and the current number
+                return {numMap[complement], i};
             }
             
-            num_map[nums[i]] = i;
+            // If not found, add the current number and its index to the map
+            numMap[nums[i]] = i;
         }
         
+        // Return an empty vector if no solution is found (though the problem guarantees one)
         return {};
     }
 };
